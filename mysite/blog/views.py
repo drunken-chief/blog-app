74e94ae761bd55cf53687a220e2d95dafe61de81
Ishan Singh <isingh@indxx.com>
2025-12-14 16:50:39 +0530

    added templates

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.http import http404
 
 def post_list(request):
     posts = Post.published.all()
     return render(request, 'blog/post/list.html', {'posts': posts})
+
+def post_detail(request, id):
+    # try:
+    #     post=Post.published.get(id=id)
+    # except Post.DoesNotExist:
+    #     raise http404("Post does not exist")
+    post = get_object_or_404(Post.published, id=id)
+    return render(request, 'blog/post/detail.html', {'post': post})
+
